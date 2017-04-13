8755cd8939fb44d37ce270565ed36ef0e3ff5059
J <1028124611@qq.com>
2017-04-14 00:12:25 +0800

    add parse request

--- src/main/java/application/uil/ParseRequest.java
+++ src/main/java/application/uil/ParseRequest.java
@@ -1,11 +1,14 @@
 package application.uil;
 
+import application.http.HttpHeaders;
+import application.http.utils.CookieMaker;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
+import org.jsoup.helper.HttpConnection;
 import org.springframework.http.HttpMethod;
 import org.springframework.util.Assert;
 
@@ -19,17 +22,31 @@ import java.util.List;
 public class ParseRequest {
     private static final Logger logger = LogManager.getLogger();
 
-    void parseFrom(String filename) throws IOException {
-        List<String> strings = FileUtils.readLines(new File(filename), "utf-8");
+    private Connection connection;
 
+    void parseFrom(String filename) throws Exception {
+        List<String> strings = FileUtils.readLines(new File(filename), "utf-8");
+        parseRequest(strings.get(0));
+
+        for (int i = 1; i < strings.size(); ++i) {
+            if (!parseHeader(strings.get(i))) {
+                parseBody(strings.get(i));
+            }
+        }
+
+        logger.info("URL:{}", JsonHelper.toJSON(connection.request().url()));
+        logger.info("Method:{}", JsonHelper.toJSON(connection.request().method()));
+        logger.info("Headers :{}", JsonHelper.toJSON(connection.request().headers()));
+        logger.info("Cookie :{}", JsonHelper.toJSON(connection.request().cookies()));
+        logger.info("Body :{}", JsonHelper.toJSON(connection.request().requestBody()));
     }
 
-    Connection.Method parseMethod(String method){
+    Connection.Method parseMethod(String method) {
         return Connection.Method.valueOf(method);
     }
 
 
-    Connection parseReqeust(String content) throws Exception {
+    void parseRequest(String content) throws Exception {
         String[] split = content.split(" ");
         Assert.isTrue(split.length >= 2);
 
@@ -37,7 +54,29 @@ public class ParseRequest {
         if (method == null)
             throw new Exception("Httpmethod parse error");
 
-        return Jsoup.connect(split[1]).method(method);
+        connection = Jsoup.connect(split[1]).method(method);
+    }
+
+    boolean parseHeader(String content) {
+
+        if (content.startsWith(HttpHeaders.COOKIE + ":")) {
+            connection.cookies(CookieMaker.make(content.substring(HttpHeaders.COOKIE.length() + 1)).data());
+            return true;
+        }
+
+        String[] split = content.split(":");
+
+        if (split.length < 2)
+            return false;
+
+        connection.header(split[0].trim(), split[1].trim());
+        return true;
+    }
+
+    void parseBody(String content) {
+        if (StringUtils.isBlank(content))
+            return;
+        connection.requestBody(content);
     }
 
 }
